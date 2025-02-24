3a99f5c774db8df041f936935f4fbe58c33c4077
Sai Teja <saitejabairoju1@gmail.com>
2025-02-24 12:34:47 +0530

    localally initialling

---
+++ Hello.java
@@ -0,0 +1,7 @@
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("basha okka sari chebithe 100 times cheppinattu ☝️");
+        System.out.println("MANKIK BHASHA");
+    }
+}
